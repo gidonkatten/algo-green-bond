c295bb38f983be8f08517cebfd526fdc4a2e75e5
Gidon <gidonkatten@gmail.com>
2021-09-19 20:51:38 +0100

    Updated trade lsig

--- assets/tradeLsig.py
+++ assets/tradeLsig.py
@@ -1,55 +1,48 @@
 import sys
 
 from pyteal import *
-from utils.params import params
-from utils.utils import parseArgs
 
 
-def contract(args):
+def contract(app_id_arg, stablecoin_id_arg, bond_id_arg, lv_arg, trade_price_arg):
     # NOTE: Lsig will remain valid until expiry
 
     # TRADE
     # check call to stateful contract is "NoOp" with "trade" arg
     ssc_call = And(
         Gtxn[0].type_enum() == TxnType.ApplicationCall,
-        Gtxn[0].application_id() == Int(args["MAIN_APP_ID"]),
+        Gtxn[0].application_id() == Int(app_id_arg),
         Gtxn[0].on_completion() == OnComplete.NoOp,
         Gtxn[0].application_args[0] == Bytes("trade"),
         Gtxn[0].fee() <= Int(1000),
         Gtxn[0].rekey_to() == Global.zero_address()
     )
 
-    # tx1 algo amount less than or equal to 1000
-    fee = And(
-        Gtxn[1].type_enum() == TxnType.Payment,
-        Gtxn[1].amount() <= Int(1000),
-        Gtxn[1].fee() <= Int(1000),
-        Gtxn[1].rekey_to() == Global.zero_address(),
-        Gtxn[1].close_remainder_to() == Global.zero_address()
-    )
+    fee = Txn.fee() <= Int(1000)
 
     # max bonds being traded verified in ssc call
     # verify bond transfer and expiry date
     bond = And(
-        Gtxn[2].type_enum() == TxnType.AssetTransfer,
-        Gtxn[2].xfer_asset() == Int(args["BOND_ID"]),
-        Gtxn[2].last_valid() < Int(args["LV"])
+        Gtxn[1].type_enum() == TxnType.AssetTransfer,
+        Gtxn[1].xfer_asset() == Int(bond_id_arg),
+        Gtxn[1].last_valid() < Int(lv_arg),
     )
     # verify transferring x bonds for y price
     stablecoin = And(
-        Gtxn[3].type_enum() == TxnType.AssetTransfer,
-        Gtxn[3].xfer_asset() == Int(args["STABLECOIN_ID"]),
-        Gtxn[3].asset_amount() == (Int(args["TRADE_PRICE"]) * Gtxn[2].asset_amount()),
-        Gtxn[3].rekey_to() == Global.zero_address(),
-        Gtxn[3].asset_close_to() == Global.zero_address()
+        Gtxn[2].type_enum() == TxnType.AssetTransfer,
+        Gtxn[2].xfer_asset() == Int(stablecoin_id_arg),
+        Gtxn[2].asset_amount() == (Int(trade_price_arg) * Gtxn[1].asset_amount()),
+        Gtxn[2].rekey_to() == Global.zero_address(),
+        Gtxn[2].asset_close_to() == Global.zero_address()
     )
 
     return ssc_call & fee & bond & stablecoin
 
 
 if __name__ == "__main__":
-    # Overwrite params if sys.argv[1] is passed
-    if len(sys.argv) > 1:
-        params = parseArgs(sys.argv[1], params)
+    app_id = int(sys.argv[1])
+    stablecoin_id = int(sys.argv[2])
+    bond_id = int(sys.argv[2])
+    lv = int(sys.argv[4])
+    trade_price = int(sys.argv[5])
 
-    print(compileTeal(contract(params), Mode.Signature, version=2))
+    print(compileTeal(contract(app_id, stablecoin_id, bond_id, lv, trade_price), Mode.Signature, version=4))
